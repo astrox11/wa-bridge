59ddef64531f870a949c78dbe5ea5c2562845abd
αѕтяσ <danielpeter0081@gmail.com>
2026-01-12 14:01:26 +0100

    remove useless function

--- api/routes/util.go
+++ api/routes/util.go
@@ -2,7 +2,6 @@ package routes
 
 import (
 	"net/http"
-	"net/url"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/gofiber/fiber/v2"
@@ -10,22 +9,6 @@ import (
 
 func UtilRoutes(app *fiber.App) {
 	api := app.Group("/util")
-
-	api.Get("/url/*", func(c *fiber.Ctx) error {
-		args := c.Params("*")
-
-		u, err := url.Parse(args)
-
-		if err != nil || u.Scheme == "" || u.Host == "" {
-			return c.Status(400).JSON(fiber.Map{
-				"status": false,
-			})
-		}
-
-		return c.JSON(fiber.Map{
-			"status": true,
-		})
-	})
 
 	api.Get("/whatsapp-news", func(c *fiber.Ctx) error {
 		res, err := http.Get("https://wabetainfo.com/")
